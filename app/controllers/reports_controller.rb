4e64cf8cb233a0c51eee6a1da7a91130432424f8
Md Sahidul Islam <sahidul03@gmail.com>
2020-03-06 01:05:01 +0900

    added dashboard

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,7 @@ class ReportsController < ApplicationController
     
     def dashboard
         @reports = WorkReceipt.select(:id, :due, :total, :discount, :created_at).group_by { |m| m.created_at.year }
+        @this_year = WorkReceipt.for_year(Date.today).select(:id, :due, :total, :discount, :created_at).group_by { |m| m.created_at.month }
     end
 
     def yearly_summary
